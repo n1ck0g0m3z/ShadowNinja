8ff8f1d3732c95a0c2f9e998187e464f7344905a
クリス <N1cK0@N1ck0-MacBook-Pro.local>
2015-11-04 11:43:31 +0900

    upload

---
+++ cocos2d/cocos/platform/android/java/bin/classes/R.java.d
@@ -0,0 +1,1 @@
+ : /Users/N1cK0/Documents/CocosGames/ShadowNinja/cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml \
--- proj.android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/N1cK0/Documents/CocosGames/ShadowNinja/proj.android/bin/AndroidManifest.xml : \
-/Users/N1cK0/Documents/CocosGames/ShadowNinja/proj.android/AndroidManifest.xml \
